fba9724f80ea300624b28715e1b945bbf08d0ac8
bipnrai054 <bipinrai054@gmail.com>
2021-05-21 10:46:56 +0545

    custom hooks created

---
+++ src/components/useForm.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useForm = () => {
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return handleChange;
+};
+
+export default useForm;
